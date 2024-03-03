681f7192f127780af3ca21f9e450cae219e21209
LoganMiller5505 <loganmiller5505@gmail.com>
2024-03-03 17:42:06 -0600

    Added new agents + total points counter
    Added greedy agent, optimistic greedy agent, and a random agent, as listed in the Source File comment. Updated weekone.py to accommodate.

--- agents.py
+++ agents.py
@@ -7,17 +7,23 @@ Requires `numpy` to be installed, and the bandits source file to be imported cor
 
 Currently contains implementations for:
     - Epsilon Greedy Agent
-
-TODO: Add implementations for:
-    - Greedy Bandit
-    - Optimistic Greedy Bandit
+    - Greedy Agent
+    - Optimistic Greedy Agent
     - Random Agent
 """
 
 import numpy as np
 from bandits import Bandit
 
-# Simple Epsilon Greedy Agent Implementation
+
+
+
+
+
+
+
+
+
 class EpsilonGreedyAgent:
     """
     Python implementation of an Epsilon Greedy Agent
@@ -28,6 +34,8 @@ class EpsilonGreedyAgent:
     ----------
     bandit : Bandit
         Associated bandit for the agent to operate on
+    total_points : float
+        Keeps a running total of all points across all n times "chooseAction" has been called
     epsilon : float
         Chance for model to pick a random (non-greedy) action. Must be between 0 and 1! Reasoning:
             - Epsilon of 0 = Greedy Model
@@ -45,9 +53,11 @@ class EpsilonGreedyAgent:
     Methods
     -------
     chooseAction()
-        Uses Epsilon-Greedy logic to select an action and realize it's associated reward.
+        Uses Epsilon-Greedy logic to select an action and realize its associated reward.
         Passes this information into the (private) updateRewards function.
     """
+
+    total_points = 0
 
     def __init__(self, bandit: Bandit, epsilon: float = 0.1) -> None:
         """
@@ -95,7 +105,7 @@ class EpsilonGreedyAgent:
 
     def chooseAction(self) -> None:
         """
-        Public method which uses Epsilon-Greedy logic to select an action and realize it's associated reward.
+        Public method which uses Epsilon-Greedy logic to select an action and realize its associated reward.
         Passes this information into the updateRewards function.
         """
         k = self.bandit.k
@@ -109,14 +119,251 @@ class EpsilonGreedyAgent:
             selected_action = self.reward_estimates.argmax()
 
         selected_reward = self.bandit.selectAction(selected_action) # Reward of selected action through bandit
+        self.total_points += selected_reward
         self.__updateRewards(selected_action, selected_reward)
     
     #TODO: Potentially add additional function to call "choose action" many (int input n) times?
     #TODO: Potentially add additional function to reset estimated values so that agent could be "reused" for different problems
         #(could potentialy be used by above proposed function)
 
-#class OptimisticGreedyAgent:
 
-#class GreedyAgent:
 
-#class RandomAgent:
+
+
+
+
+
+
+
+class OptimisticGreedyAgent:
+    """
+    Python implementation of an Optimistic Greedy Agent
+
+    ...
+
+    Attributes
+    ----------
+    bandit : Bandit
+        Associated bandit for the agent to operate on
+    total_points : float
+        Keeps a running total of all points across all n times "chooseAction" has been called
+    reward_estimates : np.array
+        Estimated value of each action's reward based on prior experience, where:
+            - index = action ID
+            - value = estimated cooresponding reward
+    reward_select_counts : np.array
+        Array that keeps track of how many times each action has been selected, where:
+            - index = action ID
+            - value = num times action has been selected
+    
+    Methods
+    -------
+    chooseAction()
+        Uses Epsilon-Greedy logic to select an action and realize its associated reward.
+        Passes this information into the (private) updateRewards function.
+    """
+
+    total_points = 0
+
+    def __init__(self, bandit: Bandit, optimistic_val: float = 50) -> None:
+        """
+        Parameters
+        ----------
+        bandit : Bandit
+            Associated bandit for the agent to operate on
+        optimistic_val : float
+            "Optimistic" value to input into model (default 50)
+            (NOTE: REQUIRES SOME LEVEL OF KNOWLEDGE ON BANDIT TO PROPERLY SET OPTIMISTIC_VAL since it, generally, must be bigger than the maximum possible reward's mean)
+        """
+        self.bandit = bandit
+        # TODO: See if there is a better way to do this, maybe in one function. I attempted this, but it lead to very loose, inaccurate floating point problems with the arrays
+        # However, for now, this provides desired behavior without issue.
+        self.reward_estimates = np.array([])
+        self.reward_select_counts = np.array([])
+        self.reward_estimates.resize(bandit.k)
+        self.reward_select_counts.resize(bandit.k)
+        if(optimistic_val==None):
+            self.reward_estimates.fill(bandit.max*2)
+        else:
+            self.reward_estimates.fill(optimistic_val)
+
+    def __updateRewards(self, selected_action: int, selected_reward: float) -> None:
+        """
+        Private method which updates "reward_estimates" and "reward_select_counts" based on the input action, reward pair representing what action the model chose and what reward it was provided.
+        Uses "Q" value updating formula described in textbook.
+
+        Parameters
+        ----------
+        selected_action : int
+            Action ID component of selected action ID/cooresponding reward pair
+        selected_reward : float
+            Reward component of selected action ID/cooresponding reward pair
+        """
+        # Using derived formulas from RL textbook
+        q = self.reward_estimates[selected_action]
+        n = self.reward_select_counts[selected_action]
+        if(n==0): #Action has NOT been selected before
+            self.reward_estimates[selected_action] = selected_reward
+        else:
+            self.reward_estimates[selected_action] = q + ( (1/n) * (selected_reward - q) )
+        self.reward_select_counts[selected_action] += 1
+
+    def chooseAction(self) -> None:
+        """
+        Public method which uses Greedy logic to select an action and realize its associated reward.
+        Passes this information into the updateRewards function.
+        """
+        selected_action = self.reward_estimates.argmax()
+
+        selected_reward = self.bandit.selectAction(selected_action) # Reward of selected action through bandit
+        self.total_points += selected_reward
+        self.__updateRewards(selected_action, selected_reward)
+    
+    #TODO: Potentially add additional function to call "choose action" many (int input n) times?
+    #TODO: Potentially add additional function to reset estimated values so that agent could be "reused" for different problems
+        #(could potentialy be used by above proposed function)
+
+
+
+
+
+
+
+
+
+
+class GreedyAgent:
+    """
+    Python implementation of a Greedy Agent
+
+    ...
+
+    Attributes
+    ----------
+    bandit : Bandit
+        Associated bandit for the agent to operate on
+    total_points : float
+        Keeps a running total of all points across all n times "chooseAction" has been called
+    reward_estimates : np.array
+        Estimated value of each action's reward based on prior experience, where:
+            - index = action ID
+            - value = estimated cooresponding reward
+    reward_select_counts : np.array
+        Array that keeps track of how many times each action has been selected, where:
+            - index = action ID
+            - value = num times action has been selected
+    
+    Methods
+    -------
+    chooseAction()
+        Uses Greedy logic to select an action and realize its associated reward.
+        Passes this information into the (private) updateRewards function.
+    """
+
+    total_points = 0
+
+    def __init__(self, bandit: Bandit) -> None:
+        """
+        Parameters
+        ----------
+        bandit : Bandit
+            Associated bandit for the agent to operate on
+        """
+        self.bandit = bandit
+        # TODO: See if there is a better way to do this, maybe in one function. I attempted this, but it lead to very loose, inaccurate floating point problems with the arrays
+        # However, for now, this provides desired behavior without issue.
+        self.reward_estimates = np.array([])
+        self.reward_select_counts = np.array([])
+        self.reward_estimates.resize(bandit.k)
+        self.reward_select_counts.resize(bandit.k)
+
+    def __updateRewards(self, selected_action: int, selected_reward: float) -> None:
+        """
+        Private method which updates "reward_estimates" and "reward_select_counts" based on the input action, reward pair representing what action the model chose and what reward it was provided.
+        Uses "Q" value updating formula described in textbook.
+
+        Parameters
+        ----------
+        selected_action : int
+            Action ID component of selected action ID/cooresponding reward pair
+        selected_reward : float
+            Reward component of selected action ID/cooresponding reward pair
+        """
+        # Using derived formulas from RL textbook
+        q = self.reward_estimates[selected_action]
+        n = self.reward_select_counts[selected_action]
+        if(n==0): #Action has NOT been selected before
+            self.reward_estimates[selected_action] = selected_reward
+        else:
+            self.reward_estimates[selected_action] = q + ( (1/n) * (selected_reward - q) )
+        self.reward_select_counts[selected_action] += 1
+
+    def chooseAction(self) -> None:
+        """
+        Public method which uses Greedy logic to select an action and realize its associated reward.
+        Passes this information into the updateRewards function.
+        """
+        selected_action = self.reward_estimates.argmax()
+
+        selected_reward = self.bandit.selectAction(selected_action) # Reward of selected action through bandit
+        self.total_points += selected_reward
+        self.__updateRewards(selected_action, selected_reward)
+    
+    #TODO: Potentially add additional function to call "choose action" many (int input n) times?
+    #TODO: Potentially add additional function to reset estimated values so that agent could be "reused" for different problems
+        #(could potentialy be used by above proposed function)
+
+
+
+
+
+
+
+
+
+
+class RandomAgent:
+    """
+    Python implementation of a Random Agent
+
+    ...
+
+    Attributes
+    ----------
+    bandit : Bandit
+        Associated bandit for the agent to operate on
+    total_points : float
+        Keeps a running total of all points across all n times "chooseAction" has been called
+    
+    Methods
+    -------
+    chooseAction()
+        Randomly select an action and realize its associated reward.
+    """
+
+    total_points = 0
+
+    def __init__(self, bandit: Bandit) -> None:
+        """
+        Parameters
+        ----------
+        bandit : Bandit
+            Associated bandit for the agent to operate on
+        """
+        self.bandit = bandit
+
+    def chooseAction(self) -> None:
+        """
+        Public method which randomly selects an action and realizes its associated reward.
+        """
+        k = self.bandit.k
+        selected_action = 0
+
+        selected_action = np.random.randint(0,k)
+
+        selected_reward = self.bandit.selectAction(selected_action) # Reward of selected action through bandit
+        self.total_points += selected_reward
+    
+    #TODO: Potentially add additional function to call "choose action" many (int input n) times?
+    #TODO: Potentially add additional function to reset estimated values so that agent could be "reused" for different problems
+        #(could potentialy be used by above proposed function)
--- weekone.py
+++ weekone.py
@@ -1,6 +1,9 @@
 import numpy as np
 from bandits import StationaryBandit
 from agents import EpsilonGreedyAgent
+from agents import GreedyAgent
+from agents import OptimisticGreedyAgent
+from agents import RandomAgent
 
 # TODO: Add futher user input, if desired
 
@@ -17,7 +20,13 @@ epsilon = float(input())
 
 # Create bandit & agent objects
 curr_bandit = StationaryBandit(k, min, max, variance)
-curr_agent = EpsilonGreedyAgent(curr_bandit, epsilon)
+
+greedy_agent = GreedyAgent(curr_bandit)
+opt_greedy_agent = OptimisticGreedyAgent(curr_bandit, 20)
+eps_greedy_agent = EpsilonGreedyAgent(curr_bandit, epsilon)
+
+random_agent = RandomAgent(curr_bandit)
+
 
 # Define runtime & output constants
 print("How many times would you like the agent to be able to choose an action? ")
@@ -25,9 +34,27 @@ n = int(input())
 print_interval = 10
 
 for i in range(0,n):
-    curr_agent.chooseAction()
+    greedy_agent.chooseAction()
+    opt_greedy_agent.chooseAction()
+    eps_greedy_agent.chooseAction()
+    random_agent.chooseAction()
     if i % print_interval == 0:
-        print(f"Current Reward Estimate at Step #{i}: {curr_agent.reward_estimates}")
+        print(f"Greedy Reward Estimate at Step #{i}: {greedy_agent.reward_estimates}")
+        print(f"Optimistic Greedy Reward Estimate at Step #{i}: {opt_greedy_agent.reward_estimates}")
+        print(f"Epsilon Greedy Reward Estimate at Step #{i}: {eps_greedy_agent.reward_estimates}")
+        print("-----------------------------------------------")
 
-print(f"Final Reward Estimate: {curr_agent.reward_estimates}")
+print(f"Final Greedy Reward Estimate: {greedy_agent.reward_estimates}")
+print(f"Final Optimistic Greedy Reward Estimate: {opt_greedy_agent.reward_estimates}")
+print(f"Final Epsilon Greedy Reward Estimate: {eps_greedy_agent.reward_estimates}")
 print(f"Actual Action Reward Values: {curr_bandit.actions}")
+print("-----------------------------------------------")
+print(f"Greedy Total Points: {greedy_agent.total_points}")
+print(f"Optimistic Greedy Total Points: {opt_greedy_agent.total_points}")
+print(f"Epsilon Greedy Total Points: {eps_greedy_agent.total_points}")
+print(f"Random Total Points: {random_agent.total_points}")
+print("-----------------------------------------------")
+print(f"Greedy Avg Points/Step: {greedy_agent.total_points/n}")
+print(f"Optimistic Greedy Avg Points/Step: {opt_greedy_agent.total_points/n}")
+print(f"Epsilon Greedy Avg Points/Step: {eps_greedy_agent.total_points/n}")
+print(f"Random Avg Points/Step: {random_agent.total_points/n}")
